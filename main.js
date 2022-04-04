2864e609d1c9d2100d20f2ded7e2bf7b78ce94c1
Angle-M <98354046+Angle-M@users.noreply.github.com>
2022-04-04 12:21:28 -0400

    Added Get,Post,Put
    They need to be adjusted

--- main.js
+++ main.js
@@ -1,29 +1,90 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const port = 3000
 
-
+//retrieve a JSON document from a file
+//Needs to be formatted
 app.get('/api/jsonBlob/<blobID>', (req, res) =>{
-  console.log(req.params);
-  app.use(bodyParser.json());
-  res.send('Got a GET request')
-})
+  var fs = require('fs');
 
+function readJSONFile(filename, callback) {
+  fs.readFile(filename, function (err, data) {
+    if(err) {
+      callback(err);
+      return;
+    }
+    try {
+      callback(null, JSON.parse(data));
+    } catch(exception) {
+      callback(exception);
+    }
+  });
+}
+  
+//create a new JSON document and save it into a file having a unique id, return the id
 app.post('/api/jsonBlob', (req, res) =>{
+  var iterator = 0; // this is going to be your identifier
+
+function addIdentifier(target){
+  target.id = iterator;
+  iterator++;
+}
+
+function loop(obj){
+
+  for(var i in obj){
+
+    var c = obj[i];        
+
+    if(typeof c === 'object'){
+
+      if(c.length === undefined){
+
+        //c is not an array
+        addIdentifier(c);
+
+      }
+
+      loop(c);
+
+    }
+
+  }
+
+}
+
+loop(json); // json is your input object
   app.write();
   res.send('Got a POST request')
 })
 
-app.put('/api/jsonBlob/<blobID>', (req, res) =>{
-  app.write();
-  res.send('Got a PUT request')
-})
 
+//update the content of a JSON document with new content and store it into the same file
+//Needs to be configured  
+app.put('/api/jsonBlob/<blobID>', (req, res) =>{
+  const fs = require('fs');
+const fileName = './file.json';
+const file = require(fileName);
+    
+file.key = "new value";
+    
+fs.writeFile(fileName, JSON.stringify(file), function writeJSON(err) {
+  if (err) return console.log(err);
+  console.log(JSON.stringify(file));
+  console.log('writing to ' + fileName);
+  res.send('Got a PUT request')
+});
+
+
+//reset the content of the JSON document
 app.delete('/api/jsonBlob/<blobID>', (req, res) =>{
   app.write();
   res.send('Got a DELETE request')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
@@ -32,6 +93,15 @@ app.listen(port, () => {
 function newFunction() {
   parse; application / x - www - form - urlencoded
 }
+
+
+
+
+
+
+
+
+
 /*
 parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
